175bbbef2278be0791993d0c84918a37a7bb7b25
PedroHenriqueFonsecaMelo <83472390+PedroHenriqueFonsecaMelo@users.noreply.github.com>
2023-03-25 16:17:07 -0300

    1.0

---
+++ flatpicker/flatpicker.js
@@ -0,0 +1,91 @@
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+      days += `<div class="dayd" id="${i}">${i}</div>`;
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  
+  renderCalendar();
+});
+
+renderCalendar();
+var elements = document.getElementsByClassName("dayd");
+var elements2 = document.querySelector('div.date h1');
+const text = elements2.childNodes[0].textContent;
+
+const buttonPressed = e => {
+    console.log(text);
+    console.log(e.target.id);  // Get ID of Clicked Element
+    
+}
+
+for (let index = 0; index < elements.length; index++) {
+    elements[index].addEventListener("click", buttonPressed);
+    
+}
+
---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*Initialize the calender component*/
+var calendar = new ej.calendars.Calendar();
+
+
+
+
+
+console.log(calendar);
+
+
+console.log("teste");
+calendar.appendTo('#element');
