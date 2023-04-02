c7eead1e15e4e54d19d6b31f3c91fd27af8d6067
PedroHenriqueFonsecaMelo <83472390+PedroHenriqueFonsecaMelo@users.noreply.github.com>
2023-04-02 13:35:57 -0300

    Carrinho js

--- js/carrinho.js
+++ js/carrinho.js
@@ -5,3 +5,58 @@ removeProductButton[i].addEventListener("click", function(event){
     console.log(event.target)
 })
 }
+
+
+const plus = document.getElementsByClassName("bx-plus"),
+minus = document.getElementsByClassName("bx-minus"),
+num = document.getElementsByClassName("num");
+
+
+for (let i = 0; i < plus.length; i++) {
+    plus[i].addEventListener("click", (e)=>{
+       
+       let a = parseInt(num[i].innerHTML);
+       a++;
+        
+        a = (a < 10) ? "0" + a : a;
+        num[i].innerText = a;
+
+        newFunction(a);
+
+        
+    });
+}
+
+for (let i = 0; i < minus.length; i++) {
+    minus[i].addEventListener("click", (e)=>{
+        let a = parseInt(num[i].innerHTML);
+        if(a > 1){
+          
+          a--;
+          
+          a = (a < 10) ? "0" + a : a;
+          num[i].innerText = a;
+      
+          newFunction(a);
+        }
+    });
+}
+
+
+
+
+function newFunction(a) {
+    td = document.getElementsByTagName("td");
+    precoBase = parseFloat(td[1].innerText.substring(3));
+    td[3].innerText = precoBase * a;
+
+    subtotal = document.getElementById("sutotal");
+    frete = document.getElementById("frete");
+    total =  document.getElementById("total");
+
+    subtotal.innerText = "Valor R$: " + td[3].innerText;
+    frete.innerText = "Valor R$: " + (parseFloat(td[3].innerText)*(2/100)).toFixed(2);
+    total.innerText = "Valor R$: " + (parseFloat(subtotal.innerHTML.substring(9))+parseFloat(frete.innerHTML.substring(9))).toFixed(2);
+
+}
+
